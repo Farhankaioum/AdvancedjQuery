94f29823cd1a1d314c3605efacf6fd84ea5e140a
Farhan Kaioum <km1472233@gmail.com>
2020-12-03 23:15:04 +0600

    Animation function added

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+   $(".red-box").fadeOut(2000);
+   $(".red-box").fadeIn(2000);
+   $(".red-box").fadeTo(2000, 0.5);
+   $(".red-box").fadeToggle();
 });
