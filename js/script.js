ed6805728ea00e815cf78904bed8f73a6f7834fa
Farhan Kaioum <km1472233@gmail.com>
2020-12-04 01:13:45 +0600

    showhidetoggle

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-   $(".red-box").fadeOut(2000);
-   $(".red-box").fadeIn(2000);
-   $(".red-box").fadeTo(2000, 0.5);
-   $(".red-box").fadeToggle();
+   //$(".red-box").fadeOut(2000);
+   //$(".red-box").fadeIn(2000);
+   //$(".red-box").fadeTo(2000, 0.5);
+   //$(".red-box").fadeToggle();
+});
+
+$(function(){
+	$(".red-box").hide(5000);
+	$(".red-box").show(3000);
+	$(".red-box").toggle(1000);
 });
